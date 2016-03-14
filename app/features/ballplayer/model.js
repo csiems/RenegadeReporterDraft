2c8b52397587fc358f9461261cbff0e66c7efb48
Christopher Siems <csiems@gmail.com>
2016-03-13 17:01:45 -0700

    create ballplayer model

--- app/features/ballplayer/model.js
+++ app/features/ballplayer/model.js
@@ -1,5 +1,20 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  //Attributes
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  photo: DS.attr('string'),
+  playerId: DS.attr('string'),
+  position: DS.attr('string'),
+  proTeam: DS.attr('string'),
+
+  //Associations
+  fakeTeam: DS.belongsTo('fake-team'),
+  user: DS.belongsTo('user'),
+
+  //Computed Properties
+  fullName: Ember.computed('firstName', 'lastName', function() {
+    return `${this.get('firstName')} ${this.get('lastName')}`;
+  })
 });
